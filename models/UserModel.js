191acd39e00f29f8e6e05491aac7930ca9071a28
Faizankhan1104 <111492455+Faizankhan1104@users.noreply.github.com>
2024-02-10 17:00:35 +0530

    Add files via upload

--- models/UserModel.js
+++ models/UserModel.js
@@ -54,7 +54,6 @@ const UserSchema = new Schema({
     {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Job',
-      unique: true,
     },
   ],
   verified: { type: Boolean, default: false },
